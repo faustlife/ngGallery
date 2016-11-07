bd927a11ad709a71ca761fc33080fa641eef3d8b
faust <faust@terrasystems.eu>
2016-11-07 17:14:16 +0200

    fix gallery size

--- src/js/ngGallery.js
+++ src/js/ngGallery.js
@@ -41,16 +41,8 @@
             '<a href="{{getImageDownloadSrc()}}" target="_blank" ng-show="showImageDownloadButton()" class="download-image"><i class="fa fa-download"></i></a>' +
             '  <a class="close-popup" ng-click="closeGallery()"><i class="fa fa-close"></i></a>' +
             '  <a class="nav-left" ng-click="prevImage()"><i class="fa fa-angle-left"></i></a>' +
-            '  <img ondragstart="return false;" draggable="false" ng-src="{{ img }}" ng-click="nextImage()" ng-show="!loading" class="effect" />' +
+            '  <img ondragstart="return false;" draggable="false" ng-src="{{ img }}"  ng-show="!loading" class="effect" />' +
             '  <a class="nav-right" ng-click="nextImage()"><i class="fa fa-angle-right"></i></a>' +
-            '  <span class="info-text">{{ index + 1 }}/{{ images.length }} - {{ description }}</span>' +
-            '  <div class="ng-thumbnails-wrapper">' +
-            '    <div class="ng-thumbnails slide-left">' +
-            '      <div ng-repeat="i in images">' +
-            '        <img ng-src="{{ i.thumb }}" ng-class="{\'active\': index === $index}" ng-click="changeImage($index)" />' +
-            '      </div>' +
-            '    </div>' +
-            '  </div>' +
             '</div>'
         );
 
@@ -217,8 +209,8 @@
                             i = index + 1,
                             s = Math.ceil(len / i);
 
-                        $thumbwrapper[0].scrollLeft = 0;
-                        $thumbwrapper[0].scrollLeft = i * item_scroll - (s * item_scroll);
+                        // $thumbwrapper[0].scrollLeft = 0;
+                        // $thumbwrapper[0].scrollLeft = i * item_scroll - (s * item_scroll);
                     }, 100);
                 };
 
